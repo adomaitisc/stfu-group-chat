13195efe7fc4a733046216b00828a8cf8a2fa7cc
adomaitisc <caadomaitis@gmail.com>
2022-11-16 11:50:52 -0500

    some updates to Home

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -8,8 +8,15 @@ const Home = () => {
         <YourGroups />
       </div>
       <div className="ml-4 w-2/3 h-full flex flex-col items-start justify-start">
-        <h1 className="text-xl ml-4">STFU Chat</h1>
+        <div className="px-4 w-full flex justify-between items-center">
+          <h1 className="text-xl">STFU Chat</h1>
+          <h1 className="text-lg font-normal text-stone-400">21 Members</h1>
+        </div>
         <div className="mt-4 bg-stone-900 h-full w-full rounded-3xl"></div>
+        <input
+          className="w-full rounded-xl py-2 px-6 font-light text-lg bg-stone-700 text-stone-200 outline-none placeholder:text-stone-400"
+          placeholder="Type a Message"
+        />
       </div>
     </div>
   );
