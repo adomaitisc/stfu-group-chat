cc85aafdc670b859d5f08f3b3aeea5134275aaba
Nicolas Alonso Froeling <nicolasalonsofroeling@Nicolass-MacBook-Pro.local>
2022-12-02 02:12:00 -0500

    bugs fixed and image added in datatbase

--- server/database/migrations/2022_12_02_063733_create_messages_table.php
+++ server/database/migrations/2022_12_02_063733_create_messages_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Messages extends Migration
+class CreateMessagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -29,6 +29,6 @@ class Messages extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('messages');
     }
 }
